0984dee96e582c4b86129de084963a8a22619b46
Tenotea <drtenotea@gmail.com>
2021-06-26 17:48:39 +0100

    feature(order-update): add functionality to the update button

--- src/pages/admin/orders/orders.jsx
+++ src/pages/admin/orders/orders.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { NavLink, BrowserRouter as Router, Switch, Route, useParams } from 'react-router-dom'
+import { NavLink, BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './orders.scss'
 import gasBottle from '../../../assets/icons/gas-bottle-dark.png'
 import Order from '../../../helpers/orders'
@@ -12,41 +12,41 @@ function getOrder(orders, id) {
 
 function getOrderDate(time) {
   const months = ['January', 'February', 'March', 'April',
-                  'May', 'June', 'July', 'August', 'September',
-                  'October', 'November', 'December' ]
+    'May', 'June', 'July', 'August', 'September',
+    'October', 'November', 'December']
   const date = new Date(time)
-  const [ day, month, year ] = [
+  const [day, month, year] = [
     date.getDate(),
     months[date.getMonth()],
     date.getFullYear()
   ]
-  return `${day} ${month}, ${year}` 
+  return `${day} ${month}, ${year}`
 }
 
-function getTimeOfOrder (time) {
+function getTimeOfOrder(time) {
   const date = new Date(time)
   let [hours, minutes] = [date.getHours(), date.getMinutes()]
   let period = hours > 12 ? 'PM' : 'AM'
-  hours = hours > 12 ? hours-12 : hours
-  hours = hours < 10 ? '0'+hours : hours
-  minutes = minutes < 10 ? '0'+minutes : minutes
+  hours = hours > 12 ? hours - 12 : hours
+  hours = hours < 10 ? '0' + hours : hours
+  minutes = minutes < 10 ? '0' + minutes : minutes
   return `${hours}:${minutes} ${period}`
 }
 
 const NoOrderInView = () => {
   return (
-  <div className="no-order">
-    <span className="material-icons icon">
-      shopping_basket
-    </span>
-    <h1>
-      No Selected Order
-    </h1>  
-  </div>
+    <div className="no-order">
+      <span className="material-icons icon">
+        shopping_basket
+      </span>
+      <h1>
+        No Selected Order
+      </h1>
+    </div>
   )
 }
 
-const OrderListItem = ({order, onClick}) => {
+const OrderListItem = ({ order, onClick }) => {
   return (
     <NavLink to={`/admin/orders/${order.orderId}`} className="order-item" activeClassName="active" onClick={() => onClick(order.orderId)}>
       <div className="order-id">
@@ -70,27 +70,50 @@ const OrderListItem = ({order, onClick}) => {
   )
 }
 
-const OrderDetail = ({label, children}) => (
+const OrderDetail = ({ label, children }) => (
   <div className="order-property">
     <h5 className="property-name"> {label} </h5>
-    <p className="property-value">
+    <p className={`property-value ${label === 'Order Status' && children}`}>
       {children}
     </p>
   </div>
 )
 
-const OrderView = ({order}) => {
+const OrderView = ({ order: currentOrder }) => {
+  const [ updating, setUpdating ] = useState(false)
+  const [updateError, setUpdateError] = useState(false)
+  const [updateSuccess, setUpdateSuccess] = useState(false)
+
+  const handleUpdateOrder = (field) => {
+    setUpdating(true)
+    setUpdateError(false)
+    setUpdateSuccess(false)
+    order.updateOrder(currentOrder.orderId, field )
+    .then(() => {
+        setUpdateSuccess(true)
+      }).catch((error) => {
+        setUpdateError(error.message)
+      }).finally(() => {
+        setUpdating(false)
+      })
+  }
+
+  useEffect(() => {
+    setUpdateError(false)
+    setUpdateSuccess(false)
+  }, [currentOrder])
+
   return (
     <section className="order-in-view">
       <div className="customer-card">
         <h3 class="customer-name">
-          { order.name }
+          {currentOrder.name}
         </h3>
         <p className="customer-address">
-          { order.address }
+          {currentOrder.address}
         </p>
         <p className="date-ordered">
-          {getOrderDate(order.timeCreated)}
+          {getOrderDate(currentOrder.timeCreated)}
         </p>
       </div>
 
@@ -98,31 +121,54 @@ const OrderView = ({order}) => {
         <h2 className="order-details-title">
           Order Details
         </h2>
+
         <div className="order-details-content">
           <OrderDetail label="Customer Name">
-            {order.name}
+            {currentOrder.name}
           </OrderDetail>
+
           <OrderDetail label="Phone Number">
-            {order.phoneNumber}
+            {currentOrder.phoneNumber}
           </OrderDetail>
+
           <OrderDetail label="No. of Kgs">
-            {order.capacity}Kg
+            {currentOrder.capacity}Kg
           </OrderDetail>
+
           <OrderDetail label="Order Status">
-            {order.status}
+            {currentOrder.status}
           </OrderDetail>
+
           <OrderDetail label="Customer Address">
-            {order.address}
+            {currentOrder.address}
           </OrderDetail>
+
           <OrderDetail label="Time Ordered">
-            {getTimeOfOrder(order.timeCreated)}
+            {getTimeOfOrder(currentOrder.timeCreated)}
           </OrderDetail>
+
           <OrderDetail label="Date Ordered">
-            {getOrderDate(order.timeCreated)}
+            {getOrderDate(currentOrder.timeCreated)}
           </OrderDetail>
-          <button className="update-order-status">
+
+          <button className={`update-order-status ${currentOrder.status} ${updating && 'updating'}`} onClick={ () => handleUpdateOrder({status: 'completed'})}>
             Mark as completed
           </button>
+
+          {
+            updateError && (
+              <div className="error-box">
+                {updateError || 'An error occcurred while updating orderStatus'}
+              </div>
+            )
+          }
+          {
+            updateSuccess && (
+              <div className="success-box">
+                Order Status updated successfully!
+              </div>
+            )
+          }
         </div>
       </div>
     </section>
@@ -133,7 +179,7 @@ const Loader = () => {
   return (
     <div className="loading-component">
       <div className="spinner">
-        
+
       </div>
       <p className="label">
         please wait
@@ -143,9 +189,9 @@ const Loader = () => {
 }
 
 export const AdminPanelOrders = () => {
-  const [ orders, setOrders ] = useState([])
-  const [ orderInView, setOrderInView ] = useState({})
-  const [ isFetchingOrders, setIsFetchingOrders ] = useState(true)
+  const [orders, setOrders] = useState([])
+  const [orderInView, setOrderInView] = useState({})
+  const [isFetchingOrders, setIsFetchingOrders] = useState(true)
 
   const handleSetOrder = (orderId) => {
     setOrderInView(getOrder(orders, orderId))
@@ -153,13 +199,13 @@ export const AdminPanelOrders = () => {
 
   useEffect(() => {
     order.getOrders()
-    .then((data) => {
-      setOrders(data)
-    }).catch(error => {
-      console.log(error)
-    }).finally(() => {
-      setIsFetchingOrders(false)
-    })
+      .then((data) => {
+        setOrders(data)
+      }).catch(error => {
+        console.log(error)
+      }).finally(() => {
+        setIsFetchingOrders(false)
+      })
   }, [])
   return (
     <Router>
@@ -170,24 +216,24 @@ export const AdminPanelOrders = () => {
           }
           <div className="order-min">
             {
-              orders.map(order => 
-                <OrderListItem 
-                  key={order.orderId} 
-                  order={order} 
-                  onClick={handleSetOrder} 
+              orders.map(order =>
+                <OrderListItem
+                  key={order.orderId}
+                  order={order}
+                  onClick={handleSetOrder}
                 />)
             }
           </div>
         </div>
         <div className="orders-view">
-            <Switch>
-              <Route path="/admin/orders/:orderId">
-                <OrderView order={orderInView} />
-              </Route>
-            </Switch>
+          <Switch>
+            <Route path="/admin/orders/:orderId">
+              <OrderView order={orderInView} />
+            </Route>
+          </Switch>
           {
             !orderInView.orderId && <NoOrderInView />
-          } 
+          }
         </div>
       </section>
     </Router>
